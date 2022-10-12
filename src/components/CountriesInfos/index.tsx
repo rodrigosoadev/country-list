92f45d323249a8173dd6394e2b2fc833369f193a
Rodrigo Soares <meuemail.rodrigodev@gmail.com>
2022-10-12 14:52:30 -0300

    update

--- src/components/CountriesInfos/index.tsx
+++ src/components/CountriesInfos/index.tsx
@@ -1,38 +1,37 @@
-import { CountriesInfosContainer, CountrySettings } from '../../Home/styles'
-import { CountriesDataProps } from '../SearchCountry/CountrySelect'
+import { CountriesInfosContainer, CountrySettings } from '../../Home/styles';
+import { CountriesDataProps } from '../SearchCountry/CountrySelect';
 
-interface CountriesInfoProps {
-    currentCountry: CountriesDataProps;
+interface CountriesInfosProps {
+  currentCountry: CountriesDataProps
 }
 
-export function CountriesInfo({ currentCountry }: CountriesInfoProps) {
-    return (
-        <CountriesInfosContainer>
-            <CountrySettings>
-                <p>
-                    Nome do país: <span>{currentCountry.name}</span>
-                </p>
-                <p>
-                    Capital: <span>{currentCountry.capital}</span>
-                </p>
-            </CountrySettings>
-            <CountrySettings>
-                <p>
-                    Moeda: <span>{currentCountry.currency}</span>
-                </p>
-                <p>
-                    Bandeira (emoji): <span>{currentCountry.emoji}</span>
-                </p>
-            </CountrySettings>
-            <CountrySettings>
-                <p>
-                    idioma(s) falado(s){''}
-                    {currentCountry.languages?.map((language) => {
-                        <span>{language.name}</span>
-                    })}
-                </p>
-            </CountrySettings>
-        </CountriesInfosContainer>
-    )
-
+export function CountriesInfos({ currentCountry }: CountriesInfosProps) {
+  return (
+    <CountriesInfosContainer>
+      <CountrySettings>
+        <p>
+          Nome do país: <span>{currentCountry.name}</span>
+        </p>
+        <p>
+          Capital: <span>{currentCountry.capital}</span>
+        </p>
+      </CountrySettings>
+      <CountrySettings>
+        <p>
+          Moeda: <span>{currentCountry.currency}</span>
+        </p>
+        <p>
+          Bandeira (emoji): <span>{currentCountry.emoji}</span>
+        </p>
+      </CountrySettings>
+      <CountrySettings>
+        <p>
+          Idioma(s) falado(s){' '}
+          {currentCountry.languages?.map((language) => (
+            <span>{language.name}</span>
+          ))}
+        </p>
+      </CountrySettings>
+    </CountriesInfosContainer>
+  )
 }
